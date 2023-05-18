5abd11d64d1f82957d5397dfd05cc94d07fcf71a
G0then <nelsonsilva19999@gmail.com>
2023-05-18 12:39:21 +0100

    Update train model

--- trainModel.py
+++ trainModel.py
@@ -2,16 +2,23 @@ import os
 import numpy as np
 import pandas as pd
 import cv2
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.layers import Dense, GlobalAveragePooling2D, Flatten
 from keras.applications.vgg16 import VGG16
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+import psutil
+
+
 
 # Define the fixed image size for input into the VGG CNN
 IMG_SIZE = (224, 224)
 
 # Load the data into a pandas dataframe
 tweet_data = pd.read_csv('F:/Toolkit/ComputacaoVisual/t4sa_text_sentiment.tsv', delimiter='\t')
+
+# Shuffle the tweet data
+tweet_data = shuffle(tweet_data)
 
 # Preprocess the images
 def preprocess_image(filename):
@@ -38,9 +45,74 @@ def preprocess_image(filename):
         print(str(e))
         return None
 
+# Train and save the model
+def train_and_save_model(X, y, model_path, first_time):
+    # Convert arrays to numpy arrays
+    X = np.array(X)
+    y = np.array(y)
+
+    print("X length: ", len(X), "   Y length: ", len(y))
+
+    # Create training, validation and testing datasets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+
+    if first_time == True:
+        # Load the pre-trained VGG16 model
+        base_model = VGG16(weights='imagenet', include_top=False, input_shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
+
+        # Add a new fully connected layers to the model for classification
+        x = base_model.output
+        #x = GlobalAveragePooling2D()(x)
+        x = Flatten()(x)
+        x = Dense(2048, activation='relu')(x)
+        x = Dense(1024, activation='relu')(x)
+        x = Dense(24, activation='relu')(x)
+        predictions = Dense(3, activation='softmax')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+
+        # Freeze the layers in the base model, so it prevents that pre-trained VGG16 CNN model weights from being updated during the training process
+        for layer in base_model.layers[:]:
+            layer.trainable = False
+
+        # Compile
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    else:
+        # Load the existing model
+        model = load_model(model_path)
+
+    # Transpose input data
+    #X_train = np.transpose(X_train, (0, 2, 3, 1))
+    #X_val = np.transpose(X_val, (0, 2, 3, 1))
+    #X_test = np.transpose(X_test, (0, 2, 3, 1))
+
+    # Fit model
+    model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
+
+    # Evaluate the performance of the model using test dataset
+    loss, accuracy = model.evaluate(X_test, y_test)
+    print('Test loss:', loss)
+    print('Test accuracy:', accuracy)
+
+    # Save model
+    model.save(model_path)
+
+
+# Set a threshold for memory usage
+memory_threshold = 500  # Specify the memory threshold in MB
+n_files_threshold = 10000 # Specify the threshold of number of files
+file_count = 0
+
 # Load the image data and labels into numpy arrays
 X = []
 y = []
+
+#Model Path
+model_path = "sentiment_model_final.h5"
+
+# First time indication
+first_time = True
+
 for tweet_id in tweet_data['TWID']:
     print("A verificar o tweet id: ", tweet_id)
     folder_path = 'F:/Toolkit/ComputacaoVisual/b-t4sa_imgs/data/'+str(tweet_id)[0:5]+'/'
@@ -57,45 +129,27 @@ for tweet_id in tweet_data['TWID']:
             y.append([tweet_data.loc[tweet_data['TWID']==tweet_id, 'NEG'].values[0],
                       tweet_data.loc[tweet_data['TWID']==tweet_id, 'NEU'].values[0],
                       tweet_data.loc[tweet_data['TWID']==tweet_id, 'POS'].values[0]])
-X = np.array(X)
-y = np.array(y)
+            file_count += 1
 
-# Create training, validation and testing datasets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=42)
+    process = psutil.Process(os.getpid())
+    memory_usage = process.memory_info().rss / 1024 / 1024  # Memory usage in MB
+    print(f"Files {file_count} - Current memory usage: {memory_usage} MB")
 
-# Load the pre-trained VGG16 model
-base_model = VGG16(weights='imagenet', include_top=False, input_shape=(IMG_SIZE[0], IMG_SIZE[1], 3))
+    # Check memory consumption every specified interval
+    # if memory_usage > memory_threshold:
+    if file_count >= n_files_threshold:
+        if first_time == True:
+            train_and_save_model(np.array(X), np.array(y), model_path, True)
+            first_time = False
+        else:
+            train_and_save_model(np.array(X), np.array(y), model_path, False)
 
-# Add a new fully connected layers to the model for classification
-x = base_model.output
-#x = GlobalAveragePooling2D()(x)
-x = Flatten()(x)
-x = Dense(2048, activation='relu')(x)
-x = Dense(1024, activation='relu')(x)
-x = Dense(24, activation='relu')(x)
-predictions = Dense(3, activation='softmax')(x)
-model = Model(inputs=base_model.input, outputs=predictions)
+        # Clear X and y to release memory
+        X = []
+        y = []
+        #Reset counter
+        file_count = 0
 
-# Freeze the layers in the base model, so it prevents that pre-trained VGG16 CNN model weights from being updated during the training process
-for layer in base_model.layers:
-    layer.trainable = False
-
-# Compile
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-# Transpose input data
-#X_train = np.transpose(X_train, (0, 2, 3, 1))
-#X_val = np.transpose(X_val, (0, 2, 3, 1))
-#X_test = np.transpose(X_test, (0, 2, 3, 1))
-
-# Fit model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
-
-# Evaluate the performance of the model using test dataset
-loss, accuracy = model.evaluate(X_test, y_test)
-print('Test loss:', loss)
-print('Test accuracy:', accuracy)
-
-# Save model
-model.save('sentiment_model.h5')
+# Train and save the final model
+#model_path = "sentiment_model_final.h5"
+#train_and_save_model(X, y, model_path)
